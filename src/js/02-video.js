7b7de05d3ff79a6f313a569868cc641478e65a7f
Єлизавета Добровольська <106765432+Elizaveta91@users.noreply.github.com>
2023-01-05 16:20:34 +0200

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,11 @@
-import Player from '@vimeo/player';
+import vimeoPlayer from '@vimeo/player';
 import throttle from 'throttleit';
     const iframe = document.querySelector('#vimeo-player');
-    const player = new VimeoPlayer(iframe);
+    const player = new vimeoPlayer(iframe);
 
     setCurrentTime();
 
-player.on('timeupdata', throttle(pauseTime, 1000)); 
+player.on('timeupdata', throttle(playedTime, 1000)); 
 
 function pauseTime(data) {
     localStorage.setItem('videoplayer-current-time', `${data.seconds}`);
